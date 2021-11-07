35c7ffe9aac4ac0ee582c86da7d448f3d3de872e
angelsflyinhell <azami@namespace.media>
2021-11-07 15:22:14 +0100

    text recognition

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const save = require('./yurei.json');
 const { getScreenshot } = require('./utils/WebIO');
 
+
+process.on('uncaughtException', function(err) {
+    console.log(`\x1b[7m\x1b[31m[ Y U R E I ${new Date().toISOString()} ]\x1b[0m ERROR @ '${err}'`);
+});
+
 function init() {
+<<<<<<< Updated upstream
     console.log('[|||||||||||||||||||||||||||||||||||||||||||||]');
     console.log('                   Y U R E I');
     console.log('[|||||||||||||||||||||||||||||||||||||||||||||]\n');
@@ -15,6 +21,20 @@ async function loadLastKey() {
         await getScreenshot(key);
         console.log(`[ Y U R E I ${new Date().toISOString()} ] Trying Key '${key}'`);
     }
+=======
+    console.log('\x1b[7m[|||||||||||||||||||||||||||||||||||||||||||||]\x1b[0m')
+    console.log('\x1b[36m                   \x1b[7mY U R E I\x1b[0m')
+    console.log('\x1b[7m[|||||||||||||||||||||||||||||||||||||||||||||]\x1b[0m\n')
+    console.log(`Scraping Content from prnt.sc (v${save.version})\n`)
+    loadLastKey()
+}
+
+async function loadLastKey() {
+    const key = stringGen(6)
+    await getScreenshot(key)
+    console.log(`\x1b[7m[ Y U R E I ${new Date().toISOString()} ]\x1b[0m Trying Key '${key}'`)
+    await loadLastKey()
+>>>>>>> Stashed changes
 }
 
 function stringGen(len) {
@@ -27,4 +47,8 @@ function stringGen(len) {
     return text;
 }
 
+<<<<<<< Updated upstream
 init();
+=======
+init()
+>>>>>>> Stashed changes
---
+++ utils/TextDetect.js
@@ -0,0 +1,17 @@
+const T = require('tesseract.js')
+const textdata = require('../textdata.json')
+const fs = require("fs");
+
+function imageToText(path) {
+    T.recognize(path, 'eng', {logger: e => {console.log(`\x1b[7m\x1b[33m[ Y U R E I ${new Date().toISOString()} ]\x1b[0m Tesseract Log: ${JSON.stringify(e)}`)}})
+        .then(result => {
+            console.log(`\x1b[7m\x1b[32m[ Y U R E I ${new Date().toISOString()} ]\x1b[0m Tesseract Result: ${JSON.stringify(result.data.text)}`)
+            textdata.scanned.push(result.data.text)
+            fs.writeFile('./textdata.json', JSON.stringify(textdata), 'utf-8', () => {
+                console.log(`\x1b[7m\x1b[32m[ Y U R E I ${new Date().toISOString()} ]\x1b[0m Saved: ${JSON.stringify(result.data.text)}`)
+            })
+        })
+}
+
+//
+module.exports = {imageToText}
