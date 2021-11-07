e6a23a34e1b030fb5fe5319fad6cc7de672236d8
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2021-11-07 14:40:05 +0100

    format files with the newest prettier version
    options (to match the current style):
    --tab-width=4 --single-quote=true --print-width=120

--- utils/WebIO.js
+++ utils/WebIO.js
@@ -1,11 +1,12 @@
-const axios = require('axios')
-const fs = require('fs')
+const axios = require('axios');
+const fs = require('fs');
 const request = require('request');
 
 async function getScreenshot(key) {
-    await axios.get(`https://prnt.sc/${key}`)
+    await axios
+        .get(`https://prnt.sc/${key}`)
         .then((response) => {
-            const sourcecode = response.data
+            const sourcecode = response.data;
             const re = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
             const s = sourcecode;
             let m;
@@ -13,30 +14,31 @@ async function getScreenshot(key) {
             do {
                 m = re.exec(s);
                 if (m) {
-                    const m1 = m[1].toString().trim()
+                    const m1 = m[1].toString().trim();
                     if (m1.startsWith('/image/')) {
-                        const now = new Date()
-                        console.log(`[ Y U R E I ${now.toISOString()} ] Found ${m1}`)
+                        const now = new Date();
+                        console.log(`[ Y U R E I ${now.toISOString()} ] Found ${m1}`);
 
                         download('https://image.prntscr.com' + m1, `./content/${m1.replace('/image/', '')}`, () => {
-                            console.log(`[ Y U R E I ${new Date().toISOString()} ] Downloaded ${m1}`)
-                        })
+                            console.log(`[ Y U R E I ${new Date().toISOString()} ] Downloaded ${m1}`);
+                        });
                         break;
                     }
                 }
             } while (m);
-        }).catch((error) => {
-            console.log(`[ Y U R E I ${new Date().toISOString()} ] Error ${error}`)
+        })
+        .catch((error) => {
+            console.log(`[ Y U R E I ${new Date().toISOString()} ] Error ${error}`);
         });
 }
 
 const download = function (uri, filename, callback) {
     request.head(uri, function (err, res, body) {
-        console.log(`[ Y U R E I ${new Date().toISOString()} ] ${res.headers['content-type']}`)
-        console.log(`[ Y U R E I ${new Date().toISOString()} ] ${res.headers['content-length']}`)
+        console.log(`[ Y U R E I ${new Date().toISOString()} ] ${res.headers['content-type']}`);
+        console.log(`[ Y U R E I ${new Date().toISOString()} ] ${res.headers['content-length']}`);
 
         request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
 };
 
-module.exports = {getScreenshot}
+module.exports = { getScreenshot };
