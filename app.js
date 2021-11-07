90080204fa7f6fd31df0a3efae2beb2ae439a9d1
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2021-11-07 15:55:17 +0100

    add prettier and fmt

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-const save = require('./yurei.json');
-const { getScreenshot } = require('./utils/WebIO');
+const save = require("./yurei.json");
+const { getScreenshot } = require("./utils/WebIO");
 
 function init() {
-    console.log('[|||||||||||||||||||||||||||||||||||||||||||||]');
-    console.log('                   Y U R E I');
-    console.log('[|||||||||||||||||||||||||||||||||||||||||||||]\n');
-    console.log(`Scraping Content from prnt.sc (v${save.version})\n`);
-    loadLastKey();
+  console.log("[|||||||||||||||||||||||||||||||||||||||||||||]");
+  console.log("                   Y U R E I");
+  console.log("[|||||||||||||||||||||||||||||||||||||||||||||]\n");
+  console.log(`Scraping Content from prnt.sc (v${save.version})\n`);
+  loadLastKey();
 }
 
 async function loadLastKey() {
-    while (true) {
-        const key = stringGen(6);
-        await getScreenshot(key);
-        console.log(`[ Y U R E I ${new Date().toISOString()} ] Trying Key '${key}'`);
-    }
+  while (true) {
+    const key = stringGen(6);
+    await getScreenshot(key);
+    console.log(
+      `[ Y U R E I ${new Date().toISOString()} ] Trying Key '${key}'`
+    );
+  }
 }
 
 function stringGen(len) {
-    let text = '';
+  let text = "";
 
-    const charset = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const charset = "abcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (let i = 0; i < len; i++) text += charset.charAt(Math.floor(Math.random() * charset.length));
+  for (let i = 0; i < len; i++)
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
 
-    return text;
+  return text;
 }
 
 init();
